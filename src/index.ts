d87f83fc5d0f54dd377559eb4b204e3ce4c7f353
Vlada Hejda <hejdavladislav@gmail.com>
2026-04-20 13:48:12 +0200

    Export Align & Position

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import Datepicker from './Datepicker';
 import type { HTMLDatepickerElement } from './Datepicker';
 import Options from './Options';
 import Day from './Day';
-import { DayOfWeek, Month } from './Helper';
+import { DayOfWeek, Month, Align, Position } from './Helper';
 import { TitleName } from './Translator';
 
 export default Datepicker;
 
 export type { HTMLDatepickerElement };
 
-export { Options, Day, DayOfWeek, Month, TitleName };
+export { Options, Day, DayOfWeek, Month, Align, Position, TitleName };
